2b88a78146329dc12705dd7e43ccd3aa3efb59a5
crux0ne <diathis47@gmail.com>
2017-02-11 13:57:40 -0800

    solid commit before changes

--- server.js
+++ server.js
@@ -6,8 +6,21 @@ var db = new sqlite3.Database('blog.db');
 var app = express();
 var PORT = 3000;
 
-app.use(bodyParser.json)
+app.use(bodyParser.json());
 
+app.get('/', function (req, res) {
+    res.send('Blog API Root');
+    //console log to test connection
+    console.log('hit root')
+});
+
+// app.get('/posts', function (req, res) {
+//    db.posts.findAll().then(function(posts) {
+//        res.json(posts);
+//    }).then(function(e) {
+//        res.status(500).send();
+//    });
+// });
 
 db.serialize(function () {
     app.listen(PORT, function () {

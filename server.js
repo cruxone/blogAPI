a6587c05fffffa435b5c1f4efe1b4d13a0b1d19d
crux0ne <diathis47@gmail.com>
2017-02-12 12:46:01 -0800

    Added a timestamp to post title, get request returns array properly

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var date = require('date-and-time');
 var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('blog.db');
 var app = express();
 var PORT = 3000;
 
 //  TODO, clean up posts array, maybe make it array of post object
-//
-//
-//
 
 app.use(bodyParser.json());
 
@@ -18,18 +16,19 @@ app.get('/', function (req, res) {
 });
 
 //get all blog posts
-//need data to be returned in an array
 app.get('/posts', function (req, res) {
-    console.log('/post end point hit')
+    console.log('/posts end point hit')
     //array initialized to contain all posts
     posts = []
+
     db.serialize(function() {
         //iterate through each post and append posts array with data
         db.each("SELECT post_id AS id, title, body FROM posts", function(err, row) {
-            posts += [row.id + " : " + row.title + " : " + row.body]
+           // posts.push({row.id:""})
+            posts.push([row.id + " : " + row.title + " : " + row.body])
         }, function(err, row) {
             //completion callback for the db each call, printing posts array
-            console.log(posts);
+            res.json(posts);
         });
     });
 });
@@ -38,12 +37,15 @@ app.get('/posts', function (req, res) {
 app.post('/post', function (req, res) {
     console.log('/post end point hit');
     var newPost = _.pick(req.body, 'title', 'body')
+    var titleParse = newPost.title.split(" ");
+    var timeStampID =  titleParse[0].toLowerCase() + date.format(new Date(), 'DD-MM-YYYY');
     db.run("INSERT INTO posts (post_id, title, body) VALUES ($id, $title, $body)", {
-        $id: 'postTest',
+        $id: timeStampID,
         $title: newPost.title,
         $body: newPost.body
     }, function(req,row) {
-        console.log(newPost + "written to database");
+        console.log(timeStampID + " written to database");
+        res.json(newPost);
     });
 });
 

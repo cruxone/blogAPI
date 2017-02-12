79dff2e5e903608660e1f23d49bd0afc2ea28420
crux0ne <diathis47@gmail.com>
2017-02-12 12:08:15 -0800

    POST request added

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var Promise = require('bluebird');
+var _ = require('underscore');
 var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('blog.db');
 var app = express();
 var PORT = 3000;
+
+//  TODO, clean up posts array, maybe make it array of post object
+//
+//
+//
 
 app.use(bodyParser.json());
 
@@ -32,8 +37,15 @@ app.get('/posts', function (req, res) {
 //put a blog post
 app.post('/post', function (req, res) {
     console.log('/post end point hit');
+    var newPost = _.pick(req.body, 'title', 'body')
+    db.run("INSERT INTO posts (post_id, title, body) VALUES ($id, $title, $body)", {
+        $id: 'postTest',
+        $title: newPost.title,
+        $body: newPost.body
+    }, function(req,row) {
+        console.log(newPost + "written to database");
+    });
 });
-
 
 db.serialize(function () {
     app.listen(PORT, function () {
